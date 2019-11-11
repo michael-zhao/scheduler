980ad5f92efd9a9b32348a7a4cefa4f9610c05dd
michael-zhao <michael.xin.zhao@gmail.com>
2019-11-11 13:20:16 -0500

    creating scheduler (ui in terminal, might turn into web app?)

---
+++ scheduler.py
@@ -0,0 +1,23 @@
+# credits to rrc84 for providing guidance of implementation
+
+import urllib.request as rq
+import argparse
+import json
+from time import sleep
+
+def download(url):
+    print(f"Fetching from: {url}")
+    response = rq.urlopen(url)
+    data = json.load(response)
+    if data["status"] != "success":
+        print("Download failed, retrying in 1 second")
+        sleep(1.0)
+        download(url)
+    else:
+        return data["data"]
+
+# information from https://classes.cornell.edu/content/SP20/api-details
+def json_courses(args):
+    acc = []
+    for sub in args.subjects:
+        url = "https://classes.cornell.edu/api/2.0/"
